058b63b6f1adf75ddebd81b2dd3f82998e0e7634
Cameron Hetzler <cameron@macyak.raleigh.ibm.com>
2020-02-27 10:58:40 -0500

    Begun editing the core file

--- linkedlist/src/linkedlist/core.clj
+++ linkedlist/src/linkedlist/core.clj
@@ -3,4 +3,17 @@
 	(:import (java.io DataOutputStream
 		ByteArrayOutputStream)))
 
+; A simple mutable linked list
+(deftype MutableLinkedList [value net]
+	clojure.lang.Seqable
+	(seq [_]
+		(lazy-seq (cons value (seq @next)))))
+
+(defn node [value]
+	(MutableLinkedList. value (atom nil)))
+
+(defn link! [node next]
+	(reset! (.next node) next)
+	next)
+
 
